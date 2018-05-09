b676e1f2c597ba75d49b1c651839ff4f6ac2b8aa
Unknown <proffy@comcast.net>
2018-05-09 09:41:00 -0700

    Adding new interface member to choose rotors

--- Component/Interfaces/IEnigmaMachine.cs
+++ Component/Interfaces/IEnigmaMachine.cs
@@ -10,6 +10,7 @@ namespace Components.Interfaces
     {
         void SetPlugboardPair(char a, char b);
         void SetPlugboard(IEnumerable<KeyValuePair<char, char>> pairs);
+        void ChooseRotors(int rotor1, int rotor2, int rotor3);
         void SetRotorDial(int rotorNumber, char rotorSetting);
         char Encode(char c);
         string Encode(string s);
